071fdd433840369b996506657feb23b84c0f5cc6
Murhussain <husseinmurashi@yahoo.com>
2023-03-14 14:56:15 +0200

    initial setup of project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': '#3F8FAC',
+        'dark-blue': '#2C3E50',
+      },
+    },
+    variants: {},
+  },
+  plugins: [],
+}
